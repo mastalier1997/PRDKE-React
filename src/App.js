1c772d1b1f0ff7e734c7cda9092d370f4ab5e1cc
jan <jan.mastalier@gmail.com>
2020-04-30 18:57:55 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Moods</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Friends</Nav.Link>
+              <Nav.Link href="#features">Profile</Nav.Link>
+            </Nav>
+            <Form inline className={"nav-form"}>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-light">Search</Button>
+            </Form>
+          </Navbar>
+
+          <div className={"title"}>
+            <h1>Moods</h1>
+              <p>Hello Mood-World!</p>
+          </div>
+
+          <div className={"post"}>
+              <form>
+                  <input/>
+                  <button>Post</button>
+              </form>
+          </div>
+
+          <div className={"timeline"}>
+              <div className={"timeline-text"}>
+                  <p>@Andi</p>
+                  <p>Mood description </p>
+              </div>
+          </div>
+      </>
+
   );
 }
 
